13bf9c7cb628c9392f9e2319c255f63ab82f921a
fuxu <fuxu.china@gmail.com>
2016-09-01 18:42:33 +0800

    Site updated: 2016-09-01 18:42:33

--- js/totop.js
+++ js/totop.js
@@ -10,3 +10,12 @@ $("#rocket").click(function() {
     });
     return false;
 });
+
+(function(){
+    var bp = document.createElement('script');
+    bp.src = '//push.zhanzhang.baidu.com/push.js';
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+
+
